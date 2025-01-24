cc62e1ef9202c161052e2a002f4a84f45c3920f6
Esteban <esteban.v.web@gmail.com>
2025-01-23 19:15:33 -0500

    Implement card body in card element

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,8 +4,10 @@ export function MovieCard({title, year, image}) {
   return (
     <article className="card">
       <img className="card__img" src={image} alt={title} title={title} width="300" height="444" />
-      <h3 className="card__title">{title}</h3>
-      <p className="card__info">{year}</p>
+      <div className="card__body">
+        <h3 className="card__title">{title}</h3>
+        <p className="card__info">{year}</p>
+      </div>
     </article>
   )
 }
