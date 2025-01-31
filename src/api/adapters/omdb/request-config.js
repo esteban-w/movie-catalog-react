d55d916e2db6f7654a8cf3dc5afb1cfd1835b7d6
Esteban <esteban.v.web@gmail.com>
2025-01-31 15:34:33 -0500

    Update OMDB API key name

--- src/api/adapters/omdb/request-config.js
+++ src/api/adapters/omdb/request-config.js
@@ -1,8 +1,8 @@
 import { responseAdapter } from "./response"
-const { VITE_API_KEY } = import.meta.env
+const { VITE_OMDB_API_KEY } = import.meta.env
 
 export function getRequestConfig(query) {
-    const queryParams = new URLSearchParams({ apikey: VITE_API_KEY, s: query })
+    const queryParams = new URLSearchParams({ apikey: VITE_OMDB_API_KEY, s: query })
     const request = new Request(`https://www.omdbapi.com/?${queryParams.toString()}`)
 
     return { request, responseAdapter };
