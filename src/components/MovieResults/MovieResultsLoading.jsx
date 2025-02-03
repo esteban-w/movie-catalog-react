baa11d48cc183b2f04df361a805a8a1c2282813a
Esteban <esteban.v.web@gmail.com>
2025-02-03 09:19:59 -0500

    Update MovieResultsLoading

--- src/components/MovieResults/MovieResultsLoading.jsx
+++ src/components/MovieResults/MovieResultsLoading.jsx
@@ -1,10 +1,15 @@
 export function MovieResultsLoading() {
   return (
-    <div className="cards">
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-    </div>
+    <>
+      <div className="results-toolbar">
+        <div className="sort-results"></div>
+      </div>
+      <div className="cards">
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+      </div>
+    </>
   )
 }
